eca1664fac014e79fdbb09a7e909bf16a7e6ff8d
ETIENNE Olivier DTSI/DESI <CBTV4480@orange.com>
2018-03-25 11:48:16 +0200

    new read func

--- web/read.php
+++ web/read.php
@@ -1,7 +1,23 @@
+<h2>Data</h2>
+<pre>
 <?php
+    $decode = $_GET['decode'];
+
     $file = 'data.txt';
  	$current = file_get_contents($file);
 	for ($i = 0; $i < strlen($current); $i++) {
-		echo $current[$i];
+		if ($decode === "1"){
+			$cur_chr = $current[$i];
+			if ($cur_chr != '\n' && $cur_chr != '\r'){ 
+				echo chr(ord($cur_chr) - 1);
+			}
+			else {
+				echo $cur_chr;
+			}
+		}
+		else {
+			echo $current[$i];
+		}
 	}
 ?>
+</pre>
